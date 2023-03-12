292c4f16a60103712f44a32f05b35c6f612b01e5
Ricardo Salambi <ricardox200@gmail.com>
2023-03-13 00:36:22 +0200

    fixed issue where program takes over an hour to generate a 1000000 row csv file

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php  
     
-    
+    ini_set('memory_limit', '-1');
 
     function generateCSV($numRecords) {
 
@@ -61,7 +61,8 @@
         $handle = fopen('output/output.csv', 'w');
     
         // Write column headers
-        fputcsv($handle, array('Id', 'Name', 'Surname', 'Initials', 'Age', 'DateOfBirth'));
+        $headers = array('Id', 'Name', 'Surname', 'Initials', 'Age', 'DateOfBirth');
+        fwrite($handle, implode(',', $headers) . "\n");
 
         // Initialize array to store generated records
         $records = array();
@@ -78,19 +79,29 @@
 
 
             // Check if record already exists
-            $recordExists = false;
 
-            foreach ($records as $record) {
-                if ($record['Name'] == $name && $record['Surname'] == $surname && $record['Age'] == $age && $record['DateOfBirth'] == $dob) {
-                    $recordExists = true;
-                    break;
-                }
-            }
+            // $recordExists = false;
+
+            // foreach ($records as $record) {
+            //     if ($record['Name'] == $name && $record['Surname'] == $surname && $record['Age'] == $age && $record['DateOfBirth'] == $dob) {
+            //         $recordExists = true;
+            //         break;
+            //     }
+            // }
     
-            // Write data to file
-            if (!$recordExists) {
-                fputcsv($handle, array($id, $name, $surname, $initials, $age, $dob));
-                $records[] = array('Name' => $name, 'Surname' => $surname, 'Age' => $age, 'DateOfBirth' => $dob);
+            // // Write data to file
+            // if (!$recordExists) {
+            //     $data = array($id, $name, $surname, $initials, $age, $dob);
+            //     fwrite($handle, implode(',', $data) . "\n");
+            //     $records[] = array('Name' => $name, 'Surname' => $surname, 'Age' => $age, 'DateOfBirth' => $dob);
+            // }
+
+            // Check if record already exists
+            if (!isset($records[$id])) {
+                // Write data to file
+                $data = array($id, $name, $surname, $initials, $age, $dob);
+                fwrite($handle, implode(',', $data) . "\n");
+                $records[$id] = array('Name' => $name, 'Surname' => $surname, 'Age' => $age, 'DateOfBirth' => $dob);
             }
         }
         
